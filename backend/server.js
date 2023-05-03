44ec30b4e2c795c4242c0b657e129de7c85330af
yu-katayama <katayu810@gmail.com>
2023-05-03 12:18:35 +0900

    テーブル二個以上の拡張

--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,48 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+/**
+ * @typedef Book
+ * @property {string} title
+ * @property {string} author
+ */
 const books = [
 	{
+		__typename: "Book",
 		title: "タイトル1",
 		author: "著者1"
 	},
 	{
+		__typename: "Book",
 		title: "タイトル2",
 		author: "著者2"
 	},
 	{
+		__typename: "Book",
 		title: "タイトル3",
 		author: "著者3"
+	},
+]
+
+/**
+ * @typedef Movie
+ * @property {string} title
+ * @property {string} director
+ */
+const movies = [
+	{
+		__typename: "Movie",
+		title: "動画1",
+		author: "著者1(動画)"
+	},
+	{
+		__typename: "Movie",
+		title: "動画2",
+		author: "著者2(動画)"
+	},
+	{
+		__typename: "Movie",
+		title: "動画3",
+		author: "著者3(動画)"
 	},
 ]
 
@@ -21,16 +52,25 @@ const typeDefs = gql`
 		author: String
 	}
 
+	type Movie {
+		title: String
+		author: String
+	}
+
+  # Union型を定義することで、複数の型をまとめて扱えるようになります
+	union SearchResult = Book | Movie
+
 	type Query {
-		test: [Book]
+		search: [SearchResult]
 	}
 `;
 
 const resolvers = {
 	Query: {
-		test: () => books,
-	}
+		search: () => [...books, ...movies],
+	},
 };
+
 
 const server = new ApolloServer({
 	typeDefs,
--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,31 +4,47 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { gql, useQuery } from '@apollo/client'
 
-const  BOOKS = gql`
-query {
-  test {
-    title
-    author
-  }
-}
-`;
+// const  BOOKS = gql`
+// query {
+//   test {
+//     title
+//     author
+//   }
+// }
+// `;
 
-console.log(BOOKS);
+/**改造後 */
+const BOOKS = gql`
+  query {
+    search {
+      ... on Book {
+        title
+        author
+      }
+      ... on Movie {
+        title
+        author
+      }
+    }
+  }
+`;
 
 function Books() {
   const { loading, error, data } = useQuery(BOOKS);
   console.log(data);
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :error</p>;
-  return data.test.map(d => {
+  if (error) return <p>Error: {error.message}</p>;
+
+  return data.search.map(d => {
     return (
       <div key={d.title}>
         <h1>{d.title}</h1>
-        <h2>{d.author}</h2>
+        <h2>{d.author || d.director}</h2>
       </div>
     )
   })
 }
+
 
 function App() {
   const [count, setCount] = useState(0)
