7a96888a1d15174e65c29f2d62feae7fea16b12a
yu-katayama <katayu810@gmail.com>
2023-05-07 18:35:18 +0900

    move folder

---
+++ front-nuxt3-apollo/app.vue
@@ -0,0 +1,24 @@
+<template>
+  <p>There are {{ data?.ships?.length || 0 }} ships.</p>
+  a{{ data }}
+</template>
+
+<script lang="ts" setup>
+const query = gql`
+  query {
+    results(author: "著者1") {
+      ... on Book {
+        title
+        author
+      }
+      ... on Movie {
+        title
+        author
+      }
+    }
+  }
+`
+const variables = { limit: 1 }
+
+const { data } = await useAsyncQuery(query, variables)
+</script>
